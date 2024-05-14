316afdcb0f13b3e9fd004504ce07ac5b93080e1f
FilippoBonanno <fbonanno15@gmail.com>
2024-05-14 17:27:46 +0200

    M-0

---
+++ js/script.js
@@ -0,0 +1,33 @@
+
+    const { createApp } = Vue
+    
+    createApp({
+    
+        data() {
+            return { 
+                todoList:[
+                    "comprare casa",
+                    "prendere le banane",
+                    "comprare una macchina ",
+                    "fare i compiti",
+                    "giocare a pallone",
+                    "uccidere flanders"
+                    ],
+               
+                    
+               
+            }
+        },
+        methods: {
+            
+    
+           
+    
+           
+        },
+        
+        mounted() {
+            
+        }
+    
+    }).mount('#app')
